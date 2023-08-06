b4130867a587cfe81d0c231e0ebdc2b6c0c7543f
anna-makarchuk <amakarchuk1820@gmail.com>
2023-08-07 01:17:11 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let colorId = null;
+
+
+btnStart.addEventListener('click', colorChange);
+btnStop.addEventListener('click', removeColorChange);
+
+function colorChange() {
+    colorId = setInterval(() => {
+        
+        document.body.style.backgroundColor = getRandomHexColor();
+        
+    }, 1000);
+    btnStart.toggleAttribute('disabled');
+    btnStop.removeAttribute('disabled');
+
+};
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+function removeColorChange() {
+    clearInterval(colorId);
+    btnStart.removeAttribute('disabled');
+    btnStop.toggleAttribute('disabled');
+    
+};
